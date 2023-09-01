2c8cfa3532df2d6a6de864415ba8e677cbbaa096
piruthuvi22 <piruthuviraj22@gmail.com>
2023-09-01 22:42:41 +0530

    location permissions done, findAddress file renamed

---
+++ functions/locationPermission.js
@@ -0,0 +1,38 @@
+import {Platform, PermissionsAndroid, ToastAndroid} from 'react-native';
+
+export const hasLocationPermission = async () => {
+  if (Platform.OS === 'android' && Platform.Version < 23) {
+    return true;
+  }
+
+  const hasPermission = await PermissionsAndroid.check(
+    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+  );
+
+  if (hasPermission) {
+    return true;
+  }
+
+  const status = await PermissionsAndroid.request(
+    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+  );
+
+  if (status === PermissionsAndroid.RESULTS.GRANTED) {
+    console.log('Location permission granted by user.');
+    return true;
+  }
+
+  if (status === PermissionsAndroid.RESULTS.DENIED) {
+    console.log('Location permission denied by user.');
+    ToastAndroid.show('Location permission denied by user.', ToastAndroid.LONG);
+  } else if (status === PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN) {
+    console.log('Location permission revoked by user.');
+    ToastAndroid.show(
+        
+      'Location permission revoked by user.',
+      ToastAndroid.LONG,
+    );
+  }
+
+  return false;
+};
--- hooks/useLocation.js
+++ hooks/useLocation.js
@@ -1,24 +1,28 @@
 import {useState, useEffect} from 'react';
 import Geolocation from 'react-native-geolocation-service';
+import {hasLocationPermission} from '../functions/locationPermission';
 
 export const getLocation = () => {
-  Geolocation.watchPosition(
-    position => {
-      // console.log('watchPosition', position.coords);
-      return position.coords;
-    },
-    error => {
-      console.error(error);
-    },
-    {
-      enableHighAccuracy: true,
-      timeout: 20000,
-      maximumAge: 1000,
-      accuracy: {
-        android: 'high',
+  let hasPermission = hasLocationPermission();
+  if (hasPermission) {
+    Geolocation.watchPosition(
+      position => {
+        // console.log('watchPosition', position.coords);
+        return position.coords;
       },
-    },
-  );
+      error => {
+        console.error(error);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 1000,
+        accuracy: {
+          android: 'high',
+        },
+      },
+    );
+  }
 };
 
 export const useLocation = () => {
@@ -26,16 +30,20 @@ export const useLocation = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const watchId = Geolocation.watchPosition(
-      position => {
-        const {latitude, longitude} = position.coords;
-        setLocation({latitude, longitude});
-      },
-      error => {
-        setError(error.message);
-      },
-      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
-    );
+    let hasPermission = hasLocationPermission();
+    let watchId;
+    if (hasPermission) {
+      watchId = Geolocation.watchPosition(
+        position => {
+          const {latitude, longitude} = position.coords;
+          setLocation({latitude, longitude});
+        },
+        error => {
+          setError(error.message);
+        },
+        {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+      );
+    }
 
     return () => {
       Geolocation.clearWatch(watchId);
