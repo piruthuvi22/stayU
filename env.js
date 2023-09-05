f45ee2695d8696dd0a4253ec1e8c4884ce2bac6d
piruthuvi22 <piruthuviraj22@gmail.com>
2023-09-05 21:49:07 +0530

    api final

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 let env = {
   // api: 'http://10.10.22.246:1000',
-  api: 'https://boardo-plb11luim-piruthuvi22.vercel.app',
+  api: 'https://stayu-piruthuvi22.vercel.app',
   // api: 'http://localhost:8082',
 };
 
